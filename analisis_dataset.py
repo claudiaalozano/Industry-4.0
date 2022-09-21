d7369ff32abc1cdab251c6466dbbd8d1b54ddb31
albabernal03 <ALBABERO@MYUAX.COM>
2022-09-21 10:31:17 +0200

    probar

--- analisis_dataset.py
+++ analisis_dataset.py
@@ -4,3 +4,9 @@ dataframe1 = pd.read_csv('corn_OHLC2013-2017.txt')
 dataframe1.to_csv('corn1.csv', index= None)
 df= pd.read_csv('corn1.csv')
 print(df)
+
+
+dataframe2= pd.read_csv('corn2013-2017.txt')
+dataframe2.to_csv('corn2.csv', index= None)
+df2= pd.read_csv('corn2.csv')
+print(df2)
--- prueba.py
+++
@@ -1,7 +0,0 @@
-from dataset import 
-import pandas as pd
-
-dataframe2= pd.read_csv('dataset.corn2013-2017')
-dataframe2.to_csv('corn2.csv', index= None)
-dd= pd.read_csv('corn1.csv')
-print(dd)
