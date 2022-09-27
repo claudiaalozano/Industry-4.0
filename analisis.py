9b31c34e8303a9def84b494b51de26d68c42b164
lauralardies <lrodrlar@myuax.com>
2022-09-27 16:05:34 +0200

    cambios

--- analisis.py
+++ analisis.py
@@ -7,28 +7,29 @@ class Analisis():
     dataframe1 = pd.read_csv('corn_OHLC2013-2017.txt')
     dataframe1.to_csv('corn1.csv', index= None)
     df= pd.read_csv('corn1.csv')
-    
+    df.columns = [["Fecha", "Precio Maíz"]]
+
     dataframe2= pd.read_csv('corn2013-2017.txt')
     dataframe2.to_csv('corn2.csv', index= None)
     df2= pd.read_csv('corn2.csv')
-      
+    df2.columns = [["Fecha", "Precio Maíz"]]
+
     dataframe3= pd.read_csv('corn2015-2017.txt')
     dataframe3.to_csv('corn3.csv', index= None)
     df3= pd.read_csv('corn3.csv')
-  
+    df3.columns = [["Fecha", "Precio Maíz"]]
+
     return(df, df2, df3)
   
   def Limpiador(): # Limpiamos el dataset para asegurarnos de no tener elementos nulos.
 
-    nombre_columnas = ["Fecha", "Precio Maiz"]
-
-    dato1= pd.read_csv('corn1.csv', names=nombre_columnas)
+    dato1= pd.read_csv('corn1.csv')
     c1=dato1.dropna()
 
-    dato2= pd.read_csv('corn2.csv', names=nombre_columnas)
+    dato2= pd.read_csv('corn2.csv')
     c2=dato2.dropna()
-
-    dato3=pd.read_csv('corn3.csv', names=nombre_columnas)
+    
+    dato3=pd.read_csv('corn3.csv')
     c3=dato3.dropna()
     
     return(c1,c2,c3)
--- clases/__pycache__/hmm.py
+++ clases/__pycache__/hmm.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 from hmmlearn import hmm
 from analisis import Analisis
 
-fichero = "https://storage.googleapis.com/kagglesdsdata/datasets/2807/4869/corn2013-2017.txt?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20220921%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20220921T134656Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=6e75c8a1116e1406d1b21aac82fc7ece8a18947f42f52c1e16e58591191d506a16f0bf681f6c082e00197ff27581f9c95f0de04bebadc2c6afecdaa9dbb1e63812954d0aa2bf51e2eceb89ae32a9e12525dde9ace41d37b5d740001337c209f20707878b9cc95838c8ce84f344e70f6fd8b3f221162aca56d7fdec284ba8f4751f6ca65b0bd8a1bf235efa2756c085e2c4e9f9f78cde3108ec6618fbffda66badc4b450690b607ff488300d518a5e4d8e364055ea5b24e86c0be3911ec6bffb070e99ae8e04a5d58f374195ba166d21bbd106a0ef85f74768cdc93e2bbd4c67309b19e243dd02a4a08a70238ca32d9b757d4126360f99b7d7f99e1208b0a4224"
+fichero = Analisis.Transformacion()
+fichero = Analisis.Limpiador()
 
-nombre_columnas = ["Fecha", "Precio Maiz"]
-
-data = pd.read_csv(fichero, names=nombre_columnas)
+data = pd.read_csv(fichero)
 data = data.dropna()
 data["Fecha"] = pd.to_datetime(data["Fecha"])
 data["Cambios Precio Maiz"] = data["Precio Maiz"].diff()
