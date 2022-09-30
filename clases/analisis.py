62a8d3bce83283d9fcc539a455c84f3a623be5d1
lauralardies <lrodrlar@myuax.com>
2022-09-30 16:30:15 +0200

    cambios

--- clases/analisis.py
+++ clases/analisis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class Analisis():
   
@@ -6,27 +7,27 @@ class Analisis():
 
     dataframe1 = pd.read_csv('corn_OHLC2013-2017.txt')
     dataframe1.to_csv('corn1.csv', index= None)
-    df= pd.read_csv('corn1.csv')
+    df = pd.read_csv('corn1.csv')
 
-    dataframe2= pd.read_csv('corn2013-2017.txt')
+    dataframe2 = pd.read_csv('corn2013-2017.txt')
     dataframe2.to_csv('corn2.csv', index= None)
-    df2= pd.read_csv('corn2.csv')
+    df2 = pd.read_csv('corn2.csv')
 
-    dataframe3= pd.read_csv('corn2015-2017.txt')
+    dataframe3 = pd.read_csv('corn2015-2017.txt')
     dataframe3.to_csv('corn3.csv', index= None)
-    df3= pd.read_csv('corn3.csv')
+    df3 = pd.read_csv('corn3.csv')
 
     return(df, df2, df3)
   
   def Limpiador(): # Limpiamos el dataset para asegurarnos de no tener elementos nulos.
 
-    dato1= pd.read_csv('corn1.csv')
-    c1=dato1.dropna()
+    dato1 = pd.read_csv('corn1.csv')
+    c1 = dato1.dropna()
 
-    dato2= pd.read_csv('corn2.csv')
-    c2=dato2.dropna()
+    dato2 = pd.read_csv('corn2.csv')
+    c2 = dato2.dropna()
     
-    dato3=pd.read_csv('corn3.csv')
-    c3=dato3.dropna()
+    dato3 = pd.read_csv('corn3.csv')
+    c3 = dato3.dropna()
     
     return(c1,c2,c3)
